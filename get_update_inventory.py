5414ddf4d54543fe761505a4d55dad2bab65e50b
Mohan Reddy <40664949+keysightgems@users.noreply.github.com>
2023-11-29 13:07:25 +0530

    Update get_update_inventory.py

--- get_update_inventory.py
+++ get_update_inventory.py
@@ -28,7 +28,7 @@ def get_devices_details():
         device_lists = [device['name'] for device in device_details]
         return device_lists
     else:
-        print(f"Error: {response.status_code}")
+        logging.error(f"Error: {response.status_code}")
         return None
 
 def get_interfaces_details():
@@ -40,7 +40,7 @@ def get_interfaces_details():
         # interface_lists = [device['name'] for device in interface_details]
         return interface_details
     else:
-        print(f"Error: {response.status_code}")
+        logging.error(f"Error: {response.status_code}")
         return None
 
 def get_devices_data():
@@ -54,7 +54,13 @@ def get_devices_data():
             interface_details = get_interfaces_details()
             for interface in interface_details:
                 if interface['device']['name'] == device_name:
-                    interface_dict.append({"name": interface['name'], "speed": interface['speed'],})                   
+                    if interface['speed'] == 100000000:
+                        interface['speed'] = "S_100G"
+                    elif interface['speed'] == 200000000:
+                        interface['speed'] = "S_200G"
+                    elif interface['speed'] == 400000000:
+                        interface['speed'] = "S_400G"
+                    interface_dict.append({"name": interface['name'], "attributes": {"speed": interface['speed']},})                   
         if device_details:
             if (device_details['custom_fields']['State']):
                 pass
@@ -65,7 +71,7 @@ def get_devices_data():
             device_data = {'Id': device_details['id'], 'Name': device_details['name'], "DeviceType": device_details['device_type']['model'], 'Manufacturer': device_details['device_type']['manufacturer']['name'], 'State': device_details['custom_fields']['State'], "interfaces": interface_dict}
             list_of_device_dicts.append(device_data)
         else:
-            print(f"Device '{device_name}' not found.")
+            logging.info(f"Device '{device_name}' not found.")
     print(json.dumps(list_of_device_dicts, indent=2))
 
 def update_devices_data(json_file):
@@ -83,7 +89,7 @@ def update_devices_data(json_file):
         url = f"{NETBOX_URL}dcim/devices/?name={device_name}" 
         response = requests.request("GET", url, headers=HEADERS)        
         device_dict = response.json()['results'][0]        
-        if device_dict['name'].lower() == device_name:
+        if device_dict['name'].lower() == device_name.lower():
             device_url = device_dict['url']
             update_data = {'name': device_dict['name'], 'device_type': device_dict['device_type']['id'], 'custom_fields': {"State": "Reserved"}}
             data = json.dumps(update_data)  
@@ -103,22 +109,49 @@ def update_devices_data(json_file):
     else:
         logging.error(f"File '{json_file}' does not exist.") 
 
+def get_devices_links():
+    interface_details = get_interfaces_details()
+    links = []
+    for interface in interface_details:
+        srcdeviceName = interface["device"]["name"]
+        src = srcdeviceName + ":" + interface["name"]
+        if interface["link_peers"] != []:
+            for peer in interface["link_peers"]:
+                dstdeviceName = peer["device"]["name"]
+                dst =  dstdeviceName + ":" + peer["name"]
+                break
+        else:
+            dst = ""
+        if src and dst:
+            links.append({"src": src, "dst": dst})
+        else:
+            links.append({})
+    links_list = [v for k, v in enumerate(links) if v not in links[:k]]
+    print(json.dumps(links_list, indent=2))
+   
+
 if __name__ == "__main__":
-    # Check the argument to determine which method to call
-    if len(sys.argv) < 3:
-        print("Error: Specify a method to execute (method_one/method_two or method_three)")
+    # Check the argument to determine which method to call    
+    if len(sys.argv) < 2:
+        logging.error("Usage: python get_update_inventory.py <command> [json_file_path]")
         sys.exit(1)
 
-    # Extract method and JSON file from command-line arguments
     method_to_execute = sys.argv[1]
-    json_file = sys.argv[2]
 
-    if method_to_execute == "get_devices_data":
-        get_devices_data()
-    elif method_to_execute == "update_devices_data":
-        update_devices_data(json_file)
+    if method_to_execute == "update_devices_data":
+        # Check if a JSON file path is provided
+        if len(sys.argv) == 3:
+            json_file = sys.argv[2]
+            update_devices_data(json_file)
+        else:
+            logging.error("Error: JSON file path is missing.")
     else:
-        print(f"Error: Unknown method '{method_to_execute}'")
-        sys.exit(1)
+        if method_to_execute == "get_devices_data":
+            get_devices_data()
+        elif method_to_execute == "get_devices_links":
+            get_devices_links()
+        else:
+            logging.error("Python method not found to execute")
 
     # update_devices_data("output.json")
+    # print(get_devices_links())
